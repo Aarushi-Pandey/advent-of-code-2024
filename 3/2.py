eba35fffea991bdbb0445e7582aba1170adef2f3
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2024-12-06 23:16:31 -0500

    fixed input problem - taking it as multiline instead of one line

--- 3/2.py
+++ 3/2.py
@@ -3,29 +3,34 @@ f = open("./logs-part2.txt", "w")
 #print(input_file.readlines())
 
 sum_of_multiply = 0
+count = 1
 
 for line in input_file:
-    print(line)
+    #print(line)
     f.write(f"line: {line}\n")
     # split by don't()
     dont_split = line.split("don't()")
-    print("don't_split:", dont_split)
-    f.write(f"don't_split:{dont_split}\n")
+    #print("don't_split:", dont_split)
+    f.write(f"don't_split:{dont_split}\n") # eg: mul(a,b)don't()mul(c,d)do()mul(e,f) => ['mul(a,b)','mul(c,d)do()mul(e,f)']
     for j, d in enumerate(dont_split):
         if j != 0:
+            f.write(f"all other parts should only take stuff after do()")
             # split by do() and only take input after it
             do_split = d.split('do()')
-            print("do_split for", d, "is", do_split)
+            #print("do_split for", d, "is", do_split)
             f.write(f"do_split for {d} is {do_split}\n")
             if len(do_split) == 1:
                 continue
             d = ''.join(do_split[1:])
             # split by mul(
             mul_split = d.split('mul(')
+            f.write(f"all other valid parts: {d}")
         else:
+            f.write(f"first part shouldn't be skipped since this is before don't")
+            f.write(f"first valid part: {d}")
             # split by mul(
             mul_split = d.split('mul(')
-        print("mul_split for", dont_split[j], "is", mul_split)
+        #print("mul_split for", dont_split[j], "is", mul_split)
         f.write(f"mul_split for {dont_split[j]} is {mul_split}\n")
 
         # for each mul_split [except first one (whatever is before mul)]
@@ -43,10 +48,11 @@ for line in input_file:
                 second_num = int(comma_split[1].split(')')[0])
 
                 sum_of_multiply += first_num * second_num
-                print("multiplying", first_num, "and", second_num)
-                print("sum_of_multiply:", sum_of_multiply)
-                f.write(f"multiplying {first_num} and {second_num}\n")
+                #print("multiplying", first_num, "and", second_num)
+                #print("sum_of_multiply:", sum_of_multiply)
+                f.write(f"{count}: multiplying {first_num} and {second_num}\n")
                 f.write(f"sum_of_multiply: {sum_of_multiply}\n")
+                count += 1
             except:
                 continue
         
