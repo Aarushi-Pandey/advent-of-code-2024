f6b49b27c60186ff392c3f86ae693f7d55eced76
Aarushi Pandey <pandey.aarushi2010@gmail.com>
2024-12-07 23:09:06 -0500

    day 4 right part 1

--- 4/1.py
+++ 4/1.py
@@ -1,4 +1,7 @@
 input_file = open("./input.txt", "r")
+f = open("./right-diagonal-check.txt", "w")
+f1 = open("./left-diagonal-check.txt", "w")
+f2 = open("./top.txt", "w")
 #print(input_file.readlines())
 
 xmas_sum = 0
@@ -24,6 +27,8 @@ for line in input_file:
 print(f"sum so far = {xmas_sum}")
 
 print("top and bottom")
+top_count = 0
+bottom_count = 0
 # sum of xmas - down
 # sum of xmas - up
 for i in range(len(input_arr[0])): # each col
@@ -33,17 +38,25 @@ for i in range(len(input_arr[0])): # each col
         vertical_str += input_arr[j][i]
 
     #print(f"vertical str: {vertical_str}")
+    f2.write(f"{vertical_str}\n")
     
     xmas_split = vertical_str.split('XMAS')
+    top_count += len(xmas_split) - 1
     #print(xmas_split)
     xmas_sum += len(xmas_split) - 1
 
     samx_split = vertical_str.split('SAMX')
+    bottom_count += len(samx_split) - 1
     #print(samx_split)
     xmas_sum += len(samx_split) - 1
 
     #print(f"sum = {xmas_sum}")
 
+print(f"top sum: {top_count}")
+print(f"bottom sum: {bottom_count}")
+
+right_xmas_sum = 0
+right_samx_sum = 0
 print(f"sum so far = {xmas_sum}")
 
 print("right diagonal")
@@ -70,24 +83,35 @@ for i in range(len(input_arr)): # each row
             diagonal_str += el
         
         #print(f"diagonal: {diagonal_str}")
+        f.write(f"{diagonal_str}\n")
         
         xmas_split = diagonal_str.split('XMAS')
+        right_xmas_sum += len(xmas_split) - 1
         #print(xmas_split)
         xmas_sum += len(xmas_split) - 1
 
         samx_split = diagonal_str.split('SAMX')
+        right_samx_sum += len(samx_split) - 1
         #print(samx_split)
         xmas_sum += len(samx_split) - 1
 
         #print(f"sum = {xmas_sum}")
 
+print(f"right xmas sum: {right_xmas_sum}")
+print(f"right samx sum: {right_samx_sum}")
+
 print(f"sum so far = {xmas_sum}")
+f.write(f"sum so far = {xmas_sum}")
 
 print("left diagonal")
+
+left_xmas_sum = 0
+left_samx_sum = 0
+
 # reverse each row and col order and do the same thing as right diagonal check
 input_arr.reverse()
 for i in range(len(input_arr)):
-    input_arr[i] = input_arr[i][::-1].strip()
+    input_arr[i] = input_arr[i].strip()
 #print(f"reversed matrix: {input_arr}")
 
 for i in range(len(input_arr)): # each row
@@ -112,15 +136,21 @@ for i in range(len(input_arr)): # each row
             diagonal_str += el
         
         #print(f"diagonal: {diagonal_str}")
+        f1.write(f"{diagonal_str}\n")
         
         xmas_split = diagonal_str.split('XMAS')
+        left_xmas_sum += len(xmas_split) - 1
         #print(xmas_split)
         xmas_sum += len(xmas_split) - 1
 
         samx_split = diagonal_str.split('SAMX')
+        left_samx_sum += len(samx_split) - 1
         #print(samx_split)
         xmas_sum += len(samx_split) - 1
 
         #print(f"sum = {xmas_sum}")
     
+print(f"left xmas sum: {left_xmas_sum}")
+print(f"left samx sum: {left_samx_sum}")
+
 print(xmas_sum)
